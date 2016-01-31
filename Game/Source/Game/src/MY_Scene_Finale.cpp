6d30547fc506594eec7889842de60c2ada7975f2
seleb <sean.s.leblanc@gmail.com>
2016-01-31 06:02:46 -0500

    placeholder for finale

---
+++ Game/Source/Game/src/MY_Scene_Finale.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <MY_Scene_Finale.h>
+
+MY_Scene_Finale::MY_Scene_Finale(Game * _game) :
+	MY_Scene_Base(_game)
+{
+
+}
+
+void MY_Scene_Finale::update(Step * _step){
+
+	MY_Scene_Base::update(_step);
+}
