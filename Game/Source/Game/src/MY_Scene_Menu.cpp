d8cfc0aeb8fffc51955163dee9fff54efb907df8
Emma Thurlow <ecthurlow@rogers.com>
2016-01-30 19:02:00 -0500

    Menu background and button layout

--- Game/Source/Game/src/MY_Scene_Menu.cpp
+++ Game/Source/Game/src/MY_Scene_Menu.cpp
@@ -9,40 +9,50 @@ MY_Scene_Menu::MY_Scene_Menu(Game * _game) :
 {
 	// Create a linear layout to contain all of our menu items
 	// remember that these elements are all going to exist in the uiLayer, so we pass in its physics world in the constructors
-	VerticalLinearLayout * layout = new VerticalLinearLayout(uiLayer->world);
-	layout->horizontalAlignment = kCENTER;
-	layout->verticalAlignment = kMIDDLE;
+	NodeUI * layout = new NodeUI(uiLayer->world);
 	// set the layout's size to 100% of its parent
 	layout->setRationalHeight(1.f);
 	layout->setRationalWidth(1.f);
+	layout->setBackgroundColour(1.f, 1.f, 1.f);
+	layout->background->mesh->pushTexture2D(MY_ResourceManager::globalAssets->getTexture("menu_bg")->texture);
 
-	// Create some text labels
-	TextLabel * gameLabel = new TextLabel(uiLayer->world, font, textShader);
-	TextLabel * exitLabel = new TextLabel(uiLayer->world, font, textShader);
+	NodeUI * gameButton = new NodeUI(uiLayer->world);
+	NodeUI * exitButton = new NodeUI(uiLayer->world);
 
-	// set the text on the labels
-	gameLabel->setText("Exorcise!");
-	exitLabel->setText("Exit");
+	gameButton->boxSizing = kCONTENT_BOX;
+	exitButton->boxSizing = kCONTENT_BOX;
 
-	// make the labels' background visible (by default both the scene's clear colour and the text colour will be black)
-	gameLabel->setBackgroundColour(1,1,1,1);
-	exitLabel->setBackgroundColour(1,1,1,1);
+	
+	gameButton->setRationalWidth(0.128f, layout);
+	gameButton->setRationalHeight(0.15f, layout);
+	exitButton->setRationalWidth(0.138f, layout);
+	exitButton->setRationalHeight(0.15f, layout);
+	
+	gameButton->setMarginLeft(0.83f);
+	exitButton->setMarginLeft(0.83f);
 
+	gameButton->setMarginBottom(0.475f);
+	exitButton->setMarginBottom(0.3f);
+
+	
+	gameButton->setBackgroundColour(1,0,1,0.5f);
+	exitButton->setBackgroundColour(1,1,0,0.5f);
+	
 	// make the labels clickable
-	gameLabel->setMouseEnabled(true);
-	exitLabel->setMouseEnabled(true);
+	gameButton->setMouseEnabled(true);
+	exitButton->setMouseEnabled(true);
 
 	// add listeners to each label, making them buttons that take the player to different scenes
-	gameLabel->eventManager.addEventListener("click", [&](sweet::Event * _event){
+	gameButton->eventManager.addEventListener("click", [&](sweet::Event * _event){
 		game->switchScene("game", false);
 	});
-	exitLabel->eventManager.addEventListener("click", [&](sweet::Event * _event){
+	exitButton->eventManager.addEventListener("click", [&](sweet::Event * _event){
 		game->exit();
 	});
 
 	// add the labels to the layout
-	layout->addChild(gameLabel);
-	layout->addChild(exitLabel);
+	layout->addChild(gameButton);
+	layout->addChild(exitButton);
 
 	// add the layout to the uiLayer
 	uiLayer->addChild(layout);
