743e6f245b2c9d969f9c81af96e1dad558ec1256
seleb <sean.s.leblanc@gmail.com>
2016-01-31 11:12:12 -0500

    more finale setup

--- Game/Source/Game/include/MY_Scene_Finale.h
+++ Game/Source/Game/include/MY_Scene_Finale.h
@@ -2,9 +2,29 @@
 
 #include <MY_Scene_MenuBase.h>
 
+#define NUM_REQUIRED_PRESSES 10
+
 class MY_Scene_Finale : public MY_Scene_MenuBase{
 public:
 	MY_Scene_Finale(Game * _game);
+	~MY_Scene_Finale();
+	
+	// number of correctly pressed buttons since start of QTE
+	unsigned long int correct;
+	// alternates between true and false on each correct button press
+	bool nextIsRight;
+
+	Timeout * timer;
+	// ripit, gripit, or sipit (in that order)
+	std::string curTarget;
+
+	sweet::EventManager * eventManager;
 
 	virtual void update(Step * _step) override;
+
+
+	void correctPress();
+	void incorrectPress();
+
+	void passTarget();
 };
